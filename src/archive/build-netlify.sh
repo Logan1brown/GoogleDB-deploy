a10c522bc715e5719534b9eb0d9f80c99c9eb297
Logan1brown <logan1brown@gmail.com>
2025-04-21 12:46:50 -0700

    chore: remove archived files
    Removed unused files that were moved to archive in previous commit.
    All files can be recovered from git history if needed.

--- src/archive/build-netlify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Upgrading pip..."
-python -m pip install --upgrade pip
-
-echo "Installing requirements..."
-python -m pip install -r requirements.txt
-
-echo "Starting Streamlit..."
-streamlit run src/dashboard/app.py
--- src/archive/netlify.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pip install -r requirements.txt
-streamlit run src/dashboard/app.py
