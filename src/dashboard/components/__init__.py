61d30338efb089806f41251b3b67ec33be94b50c
Logan1brown <logan1brown@gmail.com>
2025-05-05 10:32:48 -0700

    init

--- src/dashboard/components/__init__.py
+++ src/dashboard/components/__init__.py
@@ -1,5 +1,14 @@
 """Dashboard components module."""
 
-from .match_details import MatchDetailsManager
-from .match_details_view import render_match_details
-from .base_match_breakdown import render_match_details_section
+# Imports moved inside functions to prevent early initialization
+def get_match_details_manager():
+    from .match_details import MatchDetailsManager
+    return MatchDetailsManager
+
+def get_render_match_details():
+    from .match_details_view import render_match_details
+    return render_match_details
+
+def get_render_match_details_section():
+    from .base_match_breakdown import render_match_details_section
+    return render_match_details_section
