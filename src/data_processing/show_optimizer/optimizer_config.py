ce6a9c5c4b921c66b6458b111c12ab258754e88a
Logan1brown <logan1brown@gmail.com>
2025-06-30 21:17:44 -0700

    rm debug

--- src/data_processing/show_optimizer/optimizer_config.py
+++ src/data_processing/show_optimizer/optimizer_config.py
@@ -39,8 +39,8 @@ class OptimizerConfig:
     # Debug mode flag - will be controlled by session state
     DEBUG_MODE = False
     
-    # Maximum number of debug messages to show - increased to capture all debug output
-    MAX_DEBUG_MESSAGES = 10000
+    # Maximum number of debug messages to show - limited to avoid overwhelming the UI
+    MAX_DEBUG_MESSAGES = 100
     
     # Debug categories to filter messages
     DEBUG_CATEGORIES = {
