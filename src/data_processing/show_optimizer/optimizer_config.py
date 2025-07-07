4568a103d40dbc5dc3be2cd67a40753cb34dfe00
Logan1brown <logan1brown@gmail.com>
2025-07-07 14:53:44 -0700

    version update

--- src/data_processing/show_optimizer/optimizer_config.py
+++ src/data_processing/show_optimizer/optimizer_config.py
@@ -31,7 +31,7 @@ class OptimizerConfig:
     """Configuration for Show Optimizer scoring and analysis."""
     
     # Version information
-    VERSION = "1.0.28"
+    VERSION = "1.0.29"
     
     # Debug message counter to limit output volume
     _debug_message_count = 0
